bdb332017e54bf30df9886a22129d18909da37fb
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-12-19 01:28:52 +0300

    Create basic navbar component

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,37 +1,44 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
+import styles from './navbar.module.scss';
+import Logo from '../Logo';
 
 function navbar() {
   return (
-    <nav className="best-izmir-nav">
-      <ul className="nav-links">
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/events">
-            <a>Events</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/teams">
-            <a>Teams</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <a>Contact Us</a>
-          </Link>
-        </li>
-      </ul>
+    <nav className={styles['best-izmir-nav']}>
+      <div className="container">
+        <div className={styles['navbar-content']}>
+          <Logo width="100" height="50" />
+          <ul className={styles['nav-links']}>
+            <li>
+              <Link href="/">
+                <a className={styles['nav-link']}>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a className={styles['nav-link']}>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/events">
+                <a className={styles['nav-link']}>Events</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/teams">
+                <a className={styles['nav-link']}>Teams</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                <a className={styles['nav-link']}>Contact Us</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
