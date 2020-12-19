d230b6e25e71ca0ea34321e946bc43362b560eaf
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-12-19 22:14:19 +0300

    Code formatting

--- components/Team/TeamCard/TeamCard.jsx
+++ components/Team/TeamCard/TeamCard.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TeamCard.module.scss';
-import Link from 'next/link';
 
 function TeamCard({ imgUrl, name, duty, mail }) {
   return (
@@ -9,7 +8,9 @@ function TeamCard({ imgUrl, name, duty, mail }) {
       <img src={imgUrl} alt={name} className={styles['team-img']} />
       <div className={styles['team-name']}>{name}</div>
       <div className={styles['team-duty']}>{duty}</div>
-      <a className={styles['team-mail']} href={`mailto:${mail}`}>{mail}</a>
+      <a className={styles['team-mail']} href={`mailto:${mail}`}>
+        {mail}
+      </a>
     </div>
   );
 }
