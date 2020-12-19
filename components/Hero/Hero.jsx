62c547a3635ca1fc6b33553f42b938ee34c639a3
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-12-19 13:36:50 +0300

    Hero component finish

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Hero.module.scss';
+import Logo from '../Logo';
+
 function Hero(props) {
   return (
     <div className={styles.hero}>
       <div className={styles['hero-content']}>
-        <h1>Best Izmir</h1>
+        <Logo width="500" height="300" isDark={false}/>
+        <h1 className={styles['hero-title']}>BOARD OF EUROPEAN STUDENTS OF TECHNOLOGY</h1>
+        <h2 className={styles['hero-subtitle']}>LOCAL GROUP Izmir</h2>
       </div>
     </div>
   );
