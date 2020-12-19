d8620794dee602c3e2dec5d49c2ab16cdebf88dc
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-12-19 22:13:38 +0300

    Fix link error

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -24,34 +24,34 @@ function navbar() {
     <nav className={cn(styles['best-izmir-nav'], scrollY > 10 && styles.scrolled, transform && styles.transform)}>
       <div className="container">
         <div className={styles['navbar-content']}>
-          <Link href="#hero">
+          <Link href="/#hero">
             <a>
               <Logo width="150" height="100" isDark={scrollY > 10 ? false : true} />
             </a>
           </Link>
           <ul className={styles['nav-links']}>
             <li>
-              <Link href="#hero">
+              <Link href="/#hero">
                 <a className={styles['nav-link']}>Anasayfa</a>
               </Link>
             </li>
             <li>
-              <Link href="#about">
+              <Link href="/#about">
                 <a className={styles['nav-link']}>Hakkımızda</a>
               </Link>
             </li>
             <li>
-              <Link href="#gallery">
+              <Link href="/#gallery">
                 <a className={styles['nav-link']}>Galeri</a>
               </Link>
             </li>
             <li>
-              <Link href="#team">
+              <Link href="/#team">
                 <a className={styles['nav-link']}>Takım</a>
               </Link>
             </li>
             <li>
-              <Link href="#contact">
+              <Link href="/#contact">
                 <a className={styles['nav-link']}>İletişim</a>
               </Link>
             </li>
