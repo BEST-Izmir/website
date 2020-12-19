77c79b75cc93bce782dd191ff405bf342115589b
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-12-19 13:33:16 +0300

    Add footer skeleton

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,9 +1,33 @@
-// import PropTypes from 'prop-types';
-
+import { RiFacebookCircleLine, RiInstagramLine, RiTwitterLine } from 'react-icons/ri';
+import styles from './Footer.module.scss';
+import Link from 'next/link';
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className="container">
+        <div className={styles['footer-content']}>
+          <div className={styles.social}>
+            <Link href="http://google.com">
+              <a>
+                <RiFacebookCircleLine size="32" />
+              </a>
+            </Link>
+            <Link href="http://google.com">
+              <a>
+                <RiInstagramLine size="32" />
+              </a>
+            </Link>
+            <Link href="http://google.com">
+              <a>
+                <RiTwitterLine size="32" />
+              </a>
+            </Link>
+          </div>
+          <span className={styles.copyright}>Copyright 2020. BEST Izmir</span>
+        </div>
+      </div>
+    </footer>
+  );
 }
-
-Footer.propTypes = {};
 
 export default Footer;
